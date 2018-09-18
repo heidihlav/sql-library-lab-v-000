b582efaa47bc88ef17f5836b3e6fe9144b96ebde
Heidi Hlavinka <hchlav@gmail.com>
2018-09-18 10:01:20 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,11 +24,11 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
-  FROM authors
-  INNER JOIN subgenres
-  ON authors.id = subgenres.id
+  FROM authors;"
+  # INNER JOIN subgenres
+  # ON authors.id = subgenres.id
 
-  ;"
+  
 end
 
 def select_series_title_with_most_human_characters
