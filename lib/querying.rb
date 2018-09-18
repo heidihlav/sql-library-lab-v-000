4e7a62ba52d2a45fcd4bf338598cc07586c34484
Heidi Hlavinka <hchlav@gmail.com>
2018-09-18 10:12:18 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,7 +34,7 @@ def select_series_title_with_most_human_characters
   FROM series
   INNER JOIN characters
   ON series.id = characters.series_id
-  ;"
+  GROUP BY species;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
