3cc398f5e71df6b043ba981d3fd7873d62e3ebbb
Heidi Hlavinka <hchlav@gmail.com>
2018-09-18 10:36:46 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -40,9 +40,9 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, character_books.id
+  "SELECT characters.name, character_books.character_id
   FROM characters
   INNER JOIN character_books
-  ON characters.id = character_books.id
+  ON characters.id = character_books.character_id
   ORDER BY character_books.id DESC;"
 end
