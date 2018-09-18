32bd0cad9ed724e23abbb417ff3559792e9d3426
Heidi Hlavinka <hchlav@gmail.com>
2018-09-18 10:01:48 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,8 +24,8 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
-  FROM authors;"
-  INNER JOIN subgenres
+  FROM authors
+  INNER JOIN subgenres;"
   # ON authors.id = subgenres.id
 
 
