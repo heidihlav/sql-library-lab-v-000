ab129e1376ebeb5042098cd5281f7fe4711c4aa1
Heidi Hlavinka <hchlav@gmail.com>
2018-09-18 10:26:25 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -44,6 +44,5 @@ def select_character_names_and_number_of_books_they_are_in
   FROM characters
   INNER JOIN character_books
   ON characters.id = character_books.character_id
-  ORDER BY **** DESC
-  ;"
+  ORDER BY character_books.character_id DESC;"
 end
